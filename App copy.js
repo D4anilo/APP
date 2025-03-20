8db1d395002d633aaae0bef39959df504a4b67aa
danilo <danilomoraisportella28@gmail.com>
2025-03-20 17:08:27 -0300

    1 teste Logon

--- App copy.js
+++ App copy.js
@@ -2,16 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Image } from 'expo-image'
 import Header from './src/components/Header';
+import Semana from './src/components/Semana';
 import Adicionar from './src/components/Adicionar';
-import Login from './src/components/Login';
-import { ScrollView } from 'react-native-web';  
+import { ScrollView } from 'react-native-web';
 
 export default function App() {
   return (
     <ScrollView>
       <View style={styles.container}>
         <Header />
-        <Login />
+        <Semana />
+        <View style={styles.foto}>
+          <Image
+            style={styles.borda}
+            source={require("./assets/bordas3.png")}
+          />
+        </View>
+        <View style={styles.footer}>
+          <Adicionar />
+        </View>
+
         <StatusBar style="auto" />
       </View>
     </ScrollView>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,33 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Input, Button } from "react-native";
 import { Image } from 'expo-image'
+import { ScrollView } from "react-native-web";
 
 export default function Login() {
     return (
-        
+        <ScrollView>
+            <View style={styles.container}>
+                <Text>FAÇA SEU LOGIN</Text>
+
+                    <Input style={styles.email}>Email</Input>
+                    <Input style={styles.senha}>Senha</Input>
+
+                <Button style={styles.Logar}>Logar</Button>
+                
+
+                <View style={styles.containerGoogle}>
+                    <Image
+                    style={styles.loginGoogle}
+                    source={require("../../assets/google.png")} />
+                </View>
+
+                <Text>CASO AINDA NÃO TENHA UMA CONTA CRIE UMA. Criar uma?</Text>
+            </View>
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center'
+    }
+})
